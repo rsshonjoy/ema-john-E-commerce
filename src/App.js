bf6899c70dd169402d75e2cda64ed95482b2f445
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-26 21:15:40 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import Review from './components/Review/Review';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header />
-      <Shop />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/review">
+            <Review />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>Sorry, page not found</h1>
+    <h3>404 Error</h3>
+  </div>
+);
+
+export default NotFound;
