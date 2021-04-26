2a19f341c8c60e3c58421be0b9e044e8c57facf8
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-26 09:28:43 +0600

    add cart section

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,42 @@ import React from 'react';
 
 const Cart = (props) => {
   const { cart } = props;
-  const grandTotal = cart.reduce((total, product) => total + product.price, 0);
+  // const grandTotal = cart.reduce((total, product) => total + product.price, 0);
+
+  let total = 0;
+  for (let i = 0; i < cart.length; i += 1) {
+    const product = cart[i];
+    total += product.price;
+  }
+
+  let shipping = 0;
+  if (total > 35) {
+    shipping = 0;
+  } else if (total > 15) {
+    shipping = 4.99;
+  } else if (total > 0) {
+    shipping = 12.99;
+  }
+
+  const tax = total / 10;
+  const grandTotal = total + shipping + tax;
+
+  const formatNumber = (num) => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+  };
   return (
     <div>
       <h4>Order Summery</h4>
       <p>Items Ordered: {cart.length}</p>
-      <p>Total Price: {grandTotal}</p>
+      <p>Product Price: {formatNumber(total)}</p>
+      <p>
+        <small>Shipping Cost: {formatNumber(shipping)}</small>
+      </p>
+      <p>
+        <small>Tax + VAT: {formatNumber(tax)}</small>
+      </p>
+      <p>Total Price: {formatNumber(grandTotal)}</p>
     </div>
   );
 };
