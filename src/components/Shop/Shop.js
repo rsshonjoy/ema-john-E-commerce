299d37e21eb2abe02701a69402e8e6fb0aae27dd
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-27 18:24:27 +0600

    Sync state in multiple routes

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import styles from './Shop.module.css';
@@ -10,6 +10,18 @@ const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products] = useState(first10);
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const saveCart = getDatabaseCart();
+    const productKey = Object.keys(saveCart);
+    // const counts = productKey.map(key => saveCart[key]);
+    const cartProducts = productKey.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      product.quantity = saveCart[key];
+      return product;
+    });
+    setCart(cartProducts);
+  }, []);
 
   const handleAddProduct = (product) => {
     const toBeAddedKey = product.key;
