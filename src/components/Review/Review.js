76c5c45d7166ef2fb10c5b350b988581bf5d76f9
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-27 08:17:10 +0600

    log product detail

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
+import fakeData from '../../fakeData';
 import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
   useEffect(() => {
     const savedCart = getDatabaseCart();
     const productKeys = Object.keys(savedCart);
-    const counts = productKeys.map((key) => savedCart[key]);
-    console.log(counts);
+    const cartProduct = productKeys.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      return product;
+    });
+    console.log(cartProduct);
   }, []);
   return (
     <div>
