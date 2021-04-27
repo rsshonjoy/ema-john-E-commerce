8a380514ce263f28af4e47a7af0ce378a2dadf44
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-27 07:56:26 +0600

    consle product key

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
-const Review = () => (
-  <div>
-    <h1>This is review</h1>
-  </div>
-);
+const Review = () => {
+  useEffect(() => {
+    const savedCart = getDatabaseCart();
+    console.log(savedCart);
+  }, []);
+  return (
+    <div>
+      <h1>This is review</h1>
+    </div>
+  );
+};
 export default Review;
