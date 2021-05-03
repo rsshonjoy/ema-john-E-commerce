8ebd087bc77ec9c90563631954ce22acc547af75
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-05-03 10:25:17 +0600

    insert bulk data many data to the database

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import fakeData from '../../fakeData';
 
-const Inventory = () => (
-  <div>
-    <h1>This is inventory</h1>
-  </div>
-);
+const Inventory = () => {
+  const handleAddProduct = () => {
+    fetch('http://localhost:5000/addProduct', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(fakeData),
+    });
+  };
+  return (
+    <div>
+      <button onClick={handleAddProduct()} type="button">
+        Add Product
+      </button>
+    </div>
+  );
+};
 
 export default Inventory;
